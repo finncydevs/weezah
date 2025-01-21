5ed4913df39ab8421cbe397273111eb0ab121183
finn <finnnxptrix@gmail.com>
2025-01-21 08:44:27 +0700

    error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,14 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
-import About from "@/components/About.vue";
 import Porto from "@/components/Weather.vue";
-import Client from "@/components/Client.vue";
-import Blog from "@/components/Blog.vue";
 import Contact from "@/components/Contact.vue";
 import Footer from "@/components/Footer.vue";
 </script>
 <template>
   <Navbar />
   <Hero />
-  <!-- <About /> -->
   <Porto />
-  <Client />
-  <!-- <Blog /> -->
   <Contact />
   <Footer />
 </template>
