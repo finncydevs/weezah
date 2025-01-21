1d9897d6376d2d7265c9c7059f4cb25efd396294
finn <finnnxptrix@gmail.com>
2025-01-21 17:40:09 +0700

    finish

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,43 +8,43 @@
           <h4 class="font-semibold text-lg text-primary mb-2">Contact</h4>
           <h2 class="font-bold text-dark text-3xl mb-4">Contact Us</h2>
           <p class="font-medium text-md text-sky-500">
-           If theres anything you want to critic or say, don't hold back
+            If theres anything you want to critic or say, don't hold back
           </p>
         </div>
       </div>
       <form action="" class="w-full lg:w-2/3">
         <div
-          class="w-full lg:mx-auto justify-center shadow-white pb-6 pt-6 rounded-md shadow-sm items-center self-center bg-primary"
+          class="w-full lg:mx-auto justify-center backdrop-blur-xl border border-sky-900 pb-6 pt-6 rounded-md shadow-sm items-center self-center"
         >
           <div class="w-full px-4 mb-8">
-            <label for="name" class="text-base text-slate-900 font-bold"
+            <label for="name" class="text-base text-sky-400 font-semibold"
               >Name</label
             >
             <input
               type="text"
               id="name"
-              class="w-full bg-slate-900 border border-sky-600 text-dark p-3 rounded-md focus:outline-none"
+              class="w-full bg-transparent border border-sky-600 text-dark p-3 rounded-md focus:outline-none"
             />
-            <label for="email" class="text-base text-slate-900 font-bold"
+            <label for="email" class="text-base text-sky-400 font-semibold"
               >Email</label
             >
             <input
               type="text"
               id="email"
-              class="w-full bg-slate-900 border border-sky-600 text-dark p-3 rounded-md focus:outline-none"
+              class="w-full bg-transparent border border-sky-600 text-dark p-3 rounded-md focus:outline-none"
             />
-            <label for="pesan" class="text-base text-slate-900 font-bold"
+            <label for="pesan" class="text-base text-sky-400 font-semibold"
               >Message</label
             >
             <textarea
               id="pesan"
-              class="w-full bg-slate-900 border border-sky-600 text-dark p-3 rounded-md focus:outline-none"
+              class="w-full bg-transparent border border-sky-600 text-dark p-3 rounded-md focus:outline-none"
               rows="4"
             ></textarea>
           </div>
           <div class="w-full px-4">
             <button
-              class="w-full rounded-md bg-slate-900 border border-sky-600 hover:bg-sky-600 py-3 px-8 text-base font-semibold text-white transition duration-500 hover:opacity-80 hover:shadow-lg"
+              class="w-full rounded-md bg-transparent border border-sky-600 hover:bg-sky-600 py-3 px-8 text-base font-semibold text-sky-400 transition duration-500 hover:opacity-80 hover:shadow-lg"
             >
               Send
             </button>
